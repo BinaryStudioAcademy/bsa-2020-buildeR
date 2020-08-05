ac3e43cdd924b65d97b8ec6573eb4d08def40f16
jyuart <yura.palayda@gmail.com>
2020-08-05 17:45:47 +0300

    Add flow block, footer and make menu collapsable

--- frontend/src/app/shell/landing-page/landing-page.component.ts
+++ frontend/src/app/shell/landing-page/landing-page.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./landing-page.component.sass']
 })
 export class LandingPageComponent implements OnInit {
+  isMenuCollapsed = true;
 
   constructor() { }
 
