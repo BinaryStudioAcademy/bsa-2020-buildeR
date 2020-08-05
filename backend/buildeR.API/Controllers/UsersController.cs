be374d03ca2f499f9739747c34f1074f9cc20d23
Zhenia Borodaikevych <borodaykevich@gmail.com>
2020-08-05 14:13:23 +0300

    added users controller as an example

---
+++ backend/buildeR.API/Controllers/UsersController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using buildeR.BLL.Interfaces;
+using buildeR.DAL.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace buildeR.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public UsersController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpGet]
+        public async Task<User> Get()
+        {
+            return await _userService.GetUserById(1);
+        }
+    }
+}
