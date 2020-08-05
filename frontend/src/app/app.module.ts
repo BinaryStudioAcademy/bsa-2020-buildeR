d000cb3eb6ea40b122548dce8316a45b42ae6f4b
jyuart <yura.palayda@gmail.com>
2020-08-05 11:05:16 +0300

    Define wildcard route to redirect all path to main page

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,10 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
+import { ShellModule } from './shell/shell.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, CoreModule, SharedModule],
+  imports: [BrowserModule, AppRoutingModule, CoreModule, SharedModule, ShellModule],
   providers: [],
   bootstrap: [AppComponent],
 })
