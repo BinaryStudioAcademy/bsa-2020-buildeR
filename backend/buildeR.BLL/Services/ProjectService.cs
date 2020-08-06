da8584cb5184a00b84f9c0d38f2a09e13f558fbe
rusgumeniuk <rus.gumeniuk@gmail.com>
2020-08-06 14:25:29 +0300

    Add GetProjectsByUser() method to IProjectService and implement it in ProjectService

--- backend/buildeR.BLL/Services/ProjectService.cs
+++ backend/buildeR.BLL/Services/ProjectService.cs
@@ -1,11 +1,16 @@
 ﻿using AutoMapper;
 
 using buildeR.BLL.Services.Abstract;
+using buildeR.Common.DTO.BuildHistory;
 using buildeR.Common.DTO.Project;
+using buildeR.Common.DTO.User;
 using buildeR.DAL.Context;
 using buildeR.DAL.Entities;
 
+using Microsoft.EntityFrameworkCore;
+
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace buildeR.BLL.Services
@@ -21,9 +26,26 @@ namespace buildeR.BLL.Services
             return base.GetAsync(id, isNoTracking);
         }
 
-        public override Task<IEnumerable<ProjectDTO>> GetAllAsync()
+        public async Task<IEnumerable<ProjectInfoDTO>> GetProjectsByUser(int userId)
         {
-            return base.GetAllAsync();
+            return await Context.Projects//TODO do need to check user existence?
+                .AsNoTracking()
+                .Include(project => project.Owner)
+                .Where(project => project.OwnerId.Equals(userId))
+                .Select(project => new ProjectInfoDTO()
+                {
+                    Id = project.Id,
+                    Name = project.Name,
+                    Owner = Mapper.Map<UserDTO>(project.Owner),
+                    LastBuildHistory = Mapper.Map<BuildHistoryDTO>(
+                          Context.BuildHistories
+                            .AsNoTracking()
+                            .Where(buildHistory => buildHistory.ProjectId.Equals(project.Id))
+                            .Distinct()
+                            .Max(buildHistory => buildHistory.BuildAt))
+
+                })
+                .ToArrayAsync();
         }
     }
 }
