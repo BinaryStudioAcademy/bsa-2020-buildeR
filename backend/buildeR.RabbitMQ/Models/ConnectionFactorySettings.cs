cccb2c954dd649776eea0cea99bfcce49c988869
Vasyl <tytysh.vasyl@gmail.com>
2020-08-06 18:28:06 +0300

    Add environment variables

--- backend/buildeR.RabbitMQ/Models/ConnectionFactorySettings.cs
+++ backend/buildeR.RabbitMQ/Models/ConnectionFactorySettings.cs
@@ -6,10 +6,10 @@ namespace buildeR.RabbitMq.Models
 {
     public static class ConnectionFactorySettings
     {
-        public static string HostName { get; set; } = "localhost";
+        public static string HostName { get; set; } = Environment.GetEnvironmentVariable("RABBIT_MQ_HOST_NAME");
         public static int Host { get; set; } = 5672;
-        public static string UserName { get; set; } = "guest";
-        public static string Password { get; set; } = "guest";
+        public static string UserName { get; set; } = Environment.GetEnvironmentVariable("RABBIT_MQ_USERNAME");
+        public static string Password { get; set; } = Environment.GetEnvironmentVariable("RABBIT_MQ_PASSWORD");
         public static string VirtualHost { get; set; } = "/";
         public static TimeSpan ContinuationTimeout { get; set; } = new TimeSpan(10, 0, 0, 0);
     }
