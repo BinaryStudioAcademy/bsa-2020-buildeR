919f8ca4a44f75bedd9cbb3fcd9a5a512730df18
rusgumeniuk <rus.gumeniuk@gmail.com>
2020-08-06 13:52:21 +0300

    Implement IProjectService in ProjectService

---
+++ backend/buildeR.BLL/Services/ProjectService.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+
+using buildeR.BLL.Services.Abstract;
+using buildeR.Common.DTO.Project;
+using buildeR.DAL.Context;
+using buildeR.DAL.Entities;
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace buildeR.BLL.Services
+{
+    public sealed class ProjectService : BaseCrudService<Project, ProjectDTO, NewProjectDTO>, IProjectService
+    {
+        public ProjectService(BuilderContext context, IMapper mapper) : base(context, mapper)
+        {
+        }
+
+        public override Task<ProjectDTO> GetAsync(int id, bool isNoTracking = false)
+        {
+            return base.GetAsync(id, isNoTracking);
+        }
+
+        public override Task<IEnumerable<ProjectDTO>> GetAllAsync()
+        {
+            return base.GetAllAsync();
+        }
+    }
+}
