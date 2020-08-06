4e735a96427bda1e58e34c8df80a040e7b17c02b
rusgumeniuk <rus.gumeniuk@gmail.com>
2020-08-06 14:39:59 +0300

    Register IProjectService and ProjectService

--- backend/buildeR.API/Extensions/ServiceExtensions.cs
+++ backend/buildeR.API/Extensions/ServiceExtensions.cs
@@ -1,5 +1,7 @@
 using buildeR.BLL.Interfaces;
 using buildeR.BLL.Services;
+using buildeR.BLL.Services.Abstract;
+
 using Microsoft.Extensions.DependencyInjection;
 
 namespace buildeR.API.Extensions
@@ -9,6 +11,8 @@ namespace buildeR.API.Extensions
         public static void RegisterCustomServices(this IServiceCollection services)
         {
             services.AddScoped<IUserService, UserService>();
+
+            services.AddScoped<IProjectService, ProjectService>();
         }
     }
 }
