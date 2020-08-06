856660df5a4f2f5a8c0d9ea4cee1ba0d3cc31f98
Vasyl <tytysh.vasyl@gmail.com>
2020-08-07 00:43:28 +0300

    Added Image storage

---
+++ backend/buildeR.BLL/Interfaces/Uploads/IImageStorage.cs
@@ -0,0 +1,15 @@
+﻿using buildeR.BLL.Services.Uploads;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace buildeR.BLL.Interfaces.Uploads
+{
+    public interface IImageStorage
+    {
+        Task<string> UploadAsync(IFormFile data);
+        Task<string> GetLastPhotoAsync();
+    }
+}
---
+++ backend/buildeR.BLL/Services/Uploads/ImageStorage.cs
@@ -0,0 +1,78 @@
+﻿using buildeR.BLL.Interfaces.Uploads;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace buildeR.BLL.Services.Uploads
+{
+    public class ImageStorage : IImageStorage
+    {
+        public ImageStorage()
+        { }
+        public async System.Threading.Tasks.Task<string> UploadAsync(IFormFile data)
+        {
+            string newFileName = DateTime.Now.Ticks + "_" + Guid.NewGuid().ToString();
+
+            var path = GetPath();
+            Directory.CreateDirectory(path);
+
+            var filePath = Path.Combine(path, newFileName + ".png");
+
+            using (var stream = new FileStream(filePath, FileMode.Create))
+            {
+                await data.CopyToAsync(stream);
+            }
+
+            return await GetBase64(data);
+        }
+
+        private string GetPath()
+        {
+            string home;
+            if(Environment.OSVersion.Platform == PlatformID.Unix)
+            {
+                home = Environment.GetEnvironmentVariable("HOME");
+            }
+            else
+            {
+                home = Environment.ExpandEnvironmentVariables("%LOCALAPPDATA%");
+            }
+            
+            return home + "\\buildeR";
+        }
+
+        private async Task<string> GetBase64(IFormFile file)
+        {
+            string base64;
+            using (var ms = new MemoryStream())
+            {
+                await file.CopyToAsync(ms);
+                var fileBytes = ms.ToArray();
+                base64 = Convert.ToBase64String(fileBytes);
+            }
+
+            return base64;
+        }
+
+        public async Task<string> GetLastPhotoAsync()
+        {
+            string root = GetPath();  
+            if (!Directory.Exists(root))
+            {
+                throw new InvalidOperationException("Users do not have any pics");
+            }
+            var lastFile = Directory.GetFiles(root, "*.png", SearchOption.AllDirectories).ToList().LastOrDefault();
+            if(lastFile == null)
+                throw new InvalidOperationException("There are no pics");
+
+            byte[] imageArray = await System.IO.File.ReadAllBytesAsync(lastFile);
+            string base64ImageRepresentation = Convert.ToBase64String(imageArray);
+
+            return base64ImageRepresentation;
+        }
+    }
+}
