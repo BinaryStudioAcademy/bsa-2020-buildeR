c55bba55cc45bbddb12fe1182266298a21206ba2
Anton Matsyshyn <tosha.matsyshyn@gmail.com>
2020-08-06 13:47:22 +0300

    Set up ElasticSearch for buildeR.API

--- backend/buildeR.API/Program.cs
+++ backend/buildeR.API/Program.cs
@@ -1,5 +1,9 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Sinks.Elasticsearch;
+using System;
 
 namespace buildeR
 {
@@ -7,6 +11,7 @@ namespace buildeR
     {
         public static void Main(string[] args)
         {
+            ConfigureLogging();
             CreateHostBuilder(args).Build().Run();
         }
 
@@ -14,8 +19,41 @@ namespace buildeR
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    webBuilder.UseStartup<Startup>()
+                              .UseSerilog();
                     webBuilder.UseUrls("http://*:5050");
                 });
+
+        private static void ConfigureLogging()
+        {
+            var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile(
+                    $"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}.json",
+                    optional: true)
+                .Build();
+
+            Log.Logger = new LoggerConfiguration()
+                .Enrich.FromLogContext()
+                .Enrich.WithMachineName()
+                .WriteTo.Debug()
+                .WriteTo.Console()
+                .WriteTo.Elasticsearch(ConfigureElasticSink(configuration, environment))
+                .Enrich.WithProperty("Environment", environment)
+                .ReadFrom.Configuration(configuration)
+                .CreateLogger();
+
+            Log.Logger.Information("Serilog configuration for buileR.API is finished.");
+        }
+
+        private static ElasticsearchSinkOptions ConfigureElasticSink(IConfigurationRoot configuration, string environment)
+        { 
+            return new ElasticsearchSinkOptions(new Uri(configuration["ElasticConfiguration:Uri"]))
+            {
+                AutoRegisterTemplate = true,
+                IndexFormat = $"api.logs"
+            }; ;
+        }
     }
 }
