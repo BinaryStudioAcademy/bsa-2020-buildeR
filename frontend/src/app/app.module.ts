87a219b20a07ffd522324b67c1a423d445ee2635
Anton Matsyshyn <tosha.matsyshyn@gmail.com>
2020-08-07 15:52:42 +0300

    Change project structure. From now cron builder is declared in SharedModule

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,10 +6,6 @@ import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
 import { WorkSpaceModule } from './modules/work-space/work-space.module';
-// import { ShellModule } from './shell/shell.module';
-// import { ModalContentComponent } from './components/modal-content/modal-content.component';
-// import { TestComponent } from './components/test/test.component';
-import { ProjectModule } from './modules/project/project.module';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToastrModule } from 'ngx-toastr';
@@ -23,9 +19,7 @@ import { ToastrModule } from 'ngx-toastr';
     CoreModule,
     SharedModule,
     WorkSpaceModule,
-    // ShellModule,
     BrowserAnimationsModule,
-    ProjectModule,
     ToastrModule.forRoot()
   ],
   providers: [],
