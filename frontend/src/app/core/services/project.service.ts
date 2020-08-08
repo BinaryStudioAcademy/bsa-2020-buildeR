8cc77ca42e4d54197f2646f465e21954d2a4b10d
MaksymFilypchuk <mfilypchuk@gmail.com>
2020-08-08 16:45:27 +0300

    add methods get/create/update to project.service

--- frontend/src/app/core/services/project.service.ts
+++ frontend/src/app/core/services/project.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpService } from './http.service';
 import { ProjectInfo } from '../../shared/models/project-info';
+import { Project } from '@shared/models/project/project';
+import { NewProject } from '@shared/models/project/new-project';
 
 @Injectable({ providedIn: 'root' })
 export class ProjectService {
@@ -10,5 +12,14 @@ export class ProjectService {
 
     public getProjectsByUser(userId: number) {
       return this.httpService.getFullRequest<ProjectInfo[]>(`${this.routePrefix}/getProjectsByUserId/${userId}`);
+  }
+  public getProjectById(projectId: number) {
+    return this.httpService.getFullRequest<Project>(`${this.routePrefix}/${projectId}/settings`);
+  }
+  public createProject(newProject: NewProject) {
+    return this.httpService.postFullRequest<Project>(`${this.routePrefix}`, newProject);
+  }
+  public updateProject(project: Project){
+    return this.httpService.putFullRequest(`${this.routePrefix}`, project);
   }
 }
