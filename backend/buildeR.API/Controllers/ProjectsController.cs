693295368c9334b75d3fe3efa2bf7779410a1450
MaksymFilypchuk <mfilypchuk@gmail.com>
2020-08-08 12:07:42 +0300

    add methods to controller and service

--- backend/buildeR.API/Controllers/ProjectsController.cs
+++ backend/buildeR.API/Controllers/ProjectsController.cs
@@ -19,10 +19,29 @@ namespace buildeR.API.Controllers
             _projectService = projectService;
         }
 
-        [HttpGet("getProjectsByUserId/{userId:int}")]
+        [HttpGet("{userId:int}")]
         public async Task<IEnumerable<ProjectInfoDTO>> GetProjectsByUserId(int userId)
         {
             return await _projectService.GetProjectsByUser(userId);
+        }
+        [HttpGet("{projectId}/settings")]
+        public async Task<ProjectDTO> GetProjectById(int projectId)
+        {
+            int userId = 1;
+            return await _projectService.GetProjectByUserId(userId, projectId);
+        }
+        [HttpPost]
+        public async Task<ActionResult<ProjectDTO>> CreateProject([FromBody] NewProjectDTO dto)
+        {
+            dto.OwnerId = 1;
+            return Ok(await _projectService.CreateProject(dto));
+        }
+        [HttpPut]
+        public async Task<ActionResult<ProjectDTO>> UpdateProject([FromBody] ProjectDTO dto)
+        {
+            dto.OwnerId = 1;
+            await _projectService.UpdateAsync(dto);
+            return Ok(await _projectService.GetAsync(dto.Id));
         }
     }
 }
