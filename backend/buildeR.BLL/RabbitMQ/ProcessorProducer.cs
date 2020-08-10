2e8b17c14df6f1d47d43f77fa64d93310c11035d
Anton Matsyshyn <tosha.matsyshyn@gmail.com>
2020-08-10 18:52:06 +0300

    Add RabbitMQ  custom producer

---
+++ backend/buildeR.BLL/RabbitMQ/ProcessorProducer.cs
@@ -0,0 +1,12 @@
+﻿using buildeR.RabbitMq.Models;
+using buildeR.RabbitMq.Realization;
+using RabbitMQ.Client;
+
+namespace buildeR.BLL.RabbitMQ
+{
+    public class ProcessorProducer: Producer
+    {
+        public ProcessorProducer(IConnectionFactory connectionFactory, QueueSettings settings): base(connectionFactory, settings)
+        { }
+    }
+}
