452cb410e109346058b603ad3d542d5dd3313a9d
Anton Matsyshyn <tosha.matsyshyn@gmail.com>
2020-08-10 18:52:55 +0300

    Set up API for producing messages

--- backend/buildeR.API/Extensions/ServiceExtensions.cs
+++ backend/buildeR.API/Extensions/ServiceExtensions.cs
@@ -1,12 +1,15 @@
 using AutoMapper;
 using buildeR.BLL.Interfaces;
 using buildeR.BLL.MappingProfiles;
+using buildeR.BLL.RabbitMQ;
 using buildeR.BLL.Services;
 using buildeR.BLL.Services.Abstract;
 
 using buildeR.Common.DTO.User;
 using buildeR.DAL.Entities;
-
+using buildeR.RabbitMq.Models;
+using buildeR.RabbitMq.Realization;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System.Reflection;
 
@@ -32,6 +35,11 @@ namespace buildeR.API.Extensions
                 cfg.AddProfile<BuildStepProfile>();
             },
             Assembly.GetExecutingAssembly());
+        }
+        public static void RegisterRabbitMQ(this IServiceCollection services, IConfiguration configuration)
+        {
+            QueueSettings queueSettings = configuration.GetSection("Queues:ToProcessor").Get<QueueSettings>();
+            services.AddTransient<ProcessorProducer>(sp => new ProcessorProducer(OwnConnectionFactory.GetConnetionFactory(), queueSettings));
         }
     }
 }
