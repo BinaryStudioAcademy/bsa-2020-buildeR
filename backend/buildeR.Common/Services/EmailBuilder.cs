d73c7f70537e1efcd484bf07e75e948d7da34236
LizaPazhukova <57843676+LizaPazhukova@users.noreply.github.com>
2020-08-11 14:55:08 +0300

    add images in project

--- backend/buildeR.Common/Services/EmailBuilder.cs
+++ backend/buildeR.Common/Services/EmailBuilder.cs
@@ -1,9 +1,18 @@
 ﻿using buildeR.Common.Interfaces;
+using Microsoft.Extensions.Configuration;
 
 namespace buildeR.Common.Services
 {
     public class EmailBuilder: IEmailBuilder 
     {
+        private readonly IConfiguration _configuration;
+        private readonly string baseUrl;
+        public EmailBuilder(IConfiguration iconfig)
+        {
+            _configuration = iconfig;
+            baseUrl = _configuration.GetValue<string>("BaseUrl");
+        }
+        
         public string CreateTemplate(string title, string body)
         {
             string emailText = @$"<tr><td style=""padding: 20px 30px 30px 30px;""><table width=""100%""><tr><td>
@@ -15,11 +24,11 @@ namespace buildeR.Common.Services
         }
         private string GenerateHeader()
         {
-            return @$"<table align=""center"" width=""600"" style=""border: 1px solid #d3d3d3; color: #000000;""><tr><td><img src=""https://trello-attachments.s3.amazonaws.com/5f306ff96e7081855d9b3f97/390x108/cc17b05385e2cb5d96c9f951f6f96464/buildeR_logo_3_%282%29.png"" alt=""logo"" width=""600"" style=""display: block;""/></td></tr>";
+            return @$"<table align=""center"" width=""600"" style=""border: 1px solid #d3d3d3; color: #000000;""><tr><td><img src=""{baseUrl}/images/logo.png"" alt=""logo"" width=""600"" style=""display: block;""/></td></tr>";
         }
         private string GenerateFooter()
         {
-            return @$"<tr><td align=""center""style=""background-color:#F5F5F5;""><img src=""https://trello-attachments.s3.amazonaws.com/5f306ff96e7081855d9b3f97/377x311/e629385942f58290679dbbed09187d06/buildeR_logo_2.png"" alt=""builder"" width=""50"" height=""45"" style=""display: block;""/> &#169; 2020 buildeR </td></tr></table>";
+            return @$"<tr><td align=""center""style=""background-color:#F5F5F5;""><img src=""{baseUrl}/images/builder.png"" alt=""builder"" width=""50"" height=""45"" style=""display: block;""/> &#169; 2020 buildeR </td></tr></table>";
         }
     }
 }
