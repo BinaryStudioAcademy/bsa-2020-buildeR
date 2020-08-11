57cdf3a5e4a912fd4187e825a381131d1308a65b
LizaPazhukova <57843676+LizaPazhukova@users.noreply.github.com>
2020-08-11 17:37:28 +0300

    remove changes

--- backend/buildeR.Common/Services/EmailBuilder.cs
+++ backend/buildeR.Common/Services/EmailBuilder.cs
@@ -1,18 +1,9 @@
 ﻿using buildeR.Common.Interfaces;
-using Microsoft.Extensions.Configuration;
 
 namespace buildeR.Common.Services
 {
     public class EmailBuilder: IEmailBuilder 
-    {
-        private readonly IConfiguration _configuration;
-        private readonly string baseUrl;
-        public EmailBuilder(IConfiguration iconfig)
-        {
-            _configuration = iconfig;
-            baseUrl = _configuration.GetValue<string>("BaseUrl");
-        }
-        
+    {  
         public string CreateTemplate(string title, string body)
         {
             string emailText = @$"<tr><td style=""padding: 20px 30px 30px 30px;""><table width=""100%""><tr><td>
@@ -24,11 +15,11 @@ namespace buildeR.Common.Services
         }
         private string GenerateHeader()
         {
-            return @$"<table align=""center"" width=""600"" style=""border: 1px solid #d3d3d3; color: #000000;""><tr><td><img src=""{baseUrl}/images/logo.png"" alt=""logo"" width=""600"" style=""display: block;""/></td></tr>";
+            return @$"<table align=""center"" width=""600"" style=""border: 1px solid #d3d3d3; color: #000000;""><tr><td><img src=""https://i.imgur.com/XlVitBi.png"" alt=""logo"" width=""600"" style=""display: block;""/></td></tr>";
         }
         private string GenerateFooter()
         {
-            return @$"<tr><td align=""center""style=""background-color:#F5F5F5;""><img src=""{baseUrl}/images/builder.png"" alt=""builder"" width=""50"" height=""45"" style=""display: block;""/> &#169; 2020 buildeR </td></tr></table>";
+            return @$"<tr><td align=""center""style=""background-color:#F5F5F5;""><img src=""https://i.imgur.com/uXA6DVV.png"" alt=""builder"" width=""50"" height=""45"" style=""display: block;""/> &#169; 2020 buildeR </td></tr></table>";
         }
     }
 }
