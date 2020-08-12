28c976bee3fb3ad18424600b821aef452311be28
Zhenia Borodaikevych <borodaykevich@gmail.com>
2020-08-12 09:35:19 +0300

    added project details under current tab

--- frontend/src/app/modules/project/project-routing.module.ts
+++ frontend/src/app/modules/project/project-routing.module.ts
@@ -5,30 +5,33 @@ import { ProjectSettingsComponent } from './project-settings/project-settings.co
 import { ProjectCreateComponent } from './project-create/project-create.component';
 import { ProjectComponent } from './project.component';
 import { ProjectResolverService } from '@core/resolvers/project-resolver.service';
+import { ProjectDetailsComponent } from './project-details/project-details.component';
 const routes: Routes = [
-{
-  path: 'create',
-  component: ProjectCreateComponent
-},
-{
-  path: ':projectId',
-  component: ProjectComponent,
-  resolve: {
-    project: ProjectResolverService
+  {
+    path: 'create',
+    component: ProjectCreateComponent,
   },
-  children: [
-    {
-      path: 'settings',
-      component: ProjectSettingsComponent
-    }
-  ]
-},
+  {
+    path: ':projectId',
+    component: ProjectComponent,
+    resolve: {
+      project: ProjectResolverService,
+    },
+    children: [
+      {
+        path: 'settings',
+        component: ProjectSettingsComponent,
+      },
+      {
+        path: 'details',
+        component: ProjectDetailsComponent
+      },
+    ],
+  },
 ];
 
 @NgModule({
-  imports: [CommonModule,
-    RouterModule.forChild(routes),
-  ],
-  exports: [RouterModule]
+  imports: [CommonModule, RouterModule.forChild(routes)],
+  exports: [RouterModule],
 })
-export class ProjectRoutingModule { }
+export class ProjectRoutingModule {}
--- frontend/src/app/modules/project/project.module.ts
+++ frontend/src/app/modules/project/project.module.ts
@@ -6,9 +6,10 @@ import { ProjectSettingsComponent } from './project-settings/project-settings.co
 import { SharedModule } from '@shared/shared.module';
 import { ProjectComponent } from './project.component';
 import { LoggingTerminalComponent } from './logging-terminal/logging-terminal.component';
+import { ProjectDetailsComponent } from './project-details/project-details.component';
 
 @NgModule({
-  declarations: [ProjectComponent, ProjectCreateComponent, ProjectSettingsComponent, LoggingTerminalComponent],
+  declarations: [ProjectComponent, ProjectCreateComponent, ProjectSettingsComponent, LoggingTerminalComponent, ProjectDetailsComponent],
   imports: [ProjectRoutingModule, SharedModule],
 })
 export class ProjectModule { }
