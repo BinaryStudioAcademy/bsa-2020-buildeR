6201c4d12a649b1d5f005e1ebe627c0fc8e08732
Volodymyr <volodymyr_bilyk@outlook.com>
2020-08-13 20:48:27 +0300

    FIxed file path

--- backend/buildeR.DAL/Migrations/QuartzDB/20200812135922_RunSqlScript.cs
+++ backend/buildeR.DAL/Migrations/QuartzDB/20200812135922_RunSqlScript.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore.Migrations;
 using System.IO;
+using System.Reflection;
 
 namespace buildeR.DAL.Migrations.QuartzDB
 {
@@ -7,8 +8,8 @@ namespace buildeR.DAL.Migrations.QuartzDB
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            var sqlFile = "../buildeR.DAL/QuartzSqlScript.sql";
-            migrationBuilder.Sql(File.ReadAllText(sqlFile));
+            var path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"Scripts/QuartzSqlScript.sql");
+            migrationBuilder.Sql(File.ReadAllText(path));
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
