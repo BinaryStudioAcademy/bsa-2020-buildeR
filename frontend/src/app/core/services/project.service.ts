8d6acbe93859102cf67c483f571e04c28989f615
LizaPazhukova <57843676+LizaPazhukova@users.noreply.github.com>
2020-08-14 13:42:11 +0300

    ability to see project when click it

--- frontend/src/app/core/services/project.service.ts
+++ frontend/src/app/core/services/project.service.ts
@@ -10,21 +10,33 @@ import { HttpResponse } from '@angular/common/http';
 export class ProjectService {
   public routePrefix = '/projects';
 
-  constructor(private httpService: HttpService) { }
+  constructor(private httpService: HttpService) {}
 
-  public getProjectsByUser(userId: number): Observable<HttpResponse<ProjectInfo[]>> {
-    return this.httpService.getFullRequest<ProjectInfo[]>(`${this.routePrefix}/getProjectsByUserId/${userId}`);
+  public getProjectsByUser(
+    userId: number
+  ): Observable<HttpResponse<ProjectInfo[]>> {
+    return this.httpService.getFullRequest<ProjectInfo[]>(
+      `${this.routePrefix}/getProjectsByUserId/${userId}`
+    );
   }
   public getProjectById(projectId: number): Observable<Project> {
-    return this.httpService.getRequest<Project>(`${this.routePrefix}/${projectId}/settings`);
+    return this.httpService.getRequest<Project>(
+      `${this.routePrefix}/${projectId}/settings`
+    );
   }
   public createProject(newProject: NewProject): Observable<Project> {
-    return this.httpService.postRequest<Project>(`${this.routePrefix}`, newProject);
+    return this.httpService.postRequest<Project>(
+      `${this.routePrefix}`,
+      newProject
+    );
   }
   public updateProject(project: Project): Observable<Project> {
     return this.httpService.putRequest<Project>(`${this.routePrefix}`, project);
   }
   public startProjectBuild(projectId: number): Observable<any> {
-    return this.httpService.postRequest<any>(`${this.routePrefix}/${projectId}/build`, null);
+    return this.httpService.postRequest<any>(
+      `${this.routePrefix}/${projectId}/build`,
+      null
+    );
   }
 }
