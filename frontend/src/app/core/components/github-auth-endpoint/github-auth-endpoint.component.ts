6640ab82cdbae72da2b5e6d9a68cbbba38bf57d7
Anton Matsyshyn <tosha.matsyshyn@gmail.com>
2020-08-14 19:05:00 +0300

    Add logic to the github auth component

--- frontend/src/app/core/components/github-auth-endpoint/github-auth-endpoint.component.ts
+++ frontend/src/app/core/components/github-auth-endpoint/github-auth-endpoint.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { GithubAuthService } from '@core/services/github-auth.service';
+import { Subject } from 'rxjs';
+import { takeUntil, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-github-auth-endpoint',
   templateUrl: './github-auth-endpoint.component.html',
   styleUrls: ['./github-auth-endpoint.component.sass']
 })
-export class GithubAuthEndpointComponent implements OnInit {
+export class GithubAuthEndpointComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  private code: string;
+
+  private unsubscribe$ = new Subject<void>();
+
+  constructor(private route: ActivatedRoute, private githubService: GithubAuthService) { }
+
+  ngOnDestroy(): void {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
+  }
 
   ngOnInit(): void {
+    this.route.queryParams
+      .pipe(
+        takeUntil(this.unsubscribe$),
+        map(params => {
+        this.code = params['code'];
+
+        this.githubService.sendCodeToApi(this.code)
+          .subscribe((token) => {
+            localStorage.setItem('github_token', token);
+            return token;
+          });
+        }))
+      .subscribe(params => params);
   }
 
 }
