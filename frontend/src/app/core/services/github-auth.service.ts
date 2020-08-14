64a31ae74e4a3abe946c4f866dce67a4cf0a5e2a
Anton Matsyshyn <tosha.matsyshyn@gmail.com>
2020-08-14 19:04:27 +0300

    Change github auth service

--- frontend/src/app/core/services/github-auth.service.ts
+++ frontend/src/app/core/services/github-auth.service.ts
@@ -1,18 +1,25 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { github } from '../../../environments/github.config';
+import { Observable } from 'rxjs';
+import { HttpService } from './http.service';
 @Injectable({
   providedIn: 'root',
 })
 export class GithubAuthService {
 
-  constructor(private readonly router: Router) {
-  }
+  private apiEndpoint: string = '/auth';
+
+  constructor(private readonly router: Router, private httpService: HttpService) { }
 
   openGithubAuthWindow(): void {
     const url = github.authEndpoint + '?' +
                 'client_id=' + github.clientId + '&' +
                 'scope=' + github.requestedScope;
     window.location.href = url;
+  }
+
+  sendCodeToApi(code: string): Observable<string> {
+    return this.httpService.postRequest<string>(`${this.apiEndpoint}/github/${code}`, null);
   }
 }
