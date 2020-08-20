c91ce62476c2d2b6a2bde28a424a20da27440770
rusgumeniuk <rus.gumeniuk@gmail.com>
2020-08-20 20:01:45 +0300

    Create BuildStepsService with functions getEmptyBuildSteps, getBuildStepsByProject, createBuildStep, removeBuildStep

---
+++ frontend/src/app/core/services/build-step.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { HttpService } from '../../core/services/http.service';
+import { Observable } from 'rxjs';
+import { HttpResponse } from '@angular/common/http';
+import { EmptyBuildStep } from '../../shared/models/empty-build-step';
+import { BuildStep } from '@shared/models/build-step';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BuildStepService {
+
+  public routePrefix = '/buildSteps';
+
+  constructor(private httpService: HttpService) { }
+
+  getEmptyBuildSteps(): Observable<HttpResponse<EmptyBuildStep[]>> {
+    return this.httpService.getFullRequest<EmptyBuildStep[]>(`${this.routePrefix}/getEmptyBuildSteps`);
+  }
+
+  getBuildStepsByProject(projectId: number): Observable<HttpResponse<BuildStep[]>>{
+    return this.httpService.getFullRequest<BuildStep[]>(`${this.routePrefix}/project/${projectId}`);
+  }
+
+  createBuildStep(buildStep: BuildStep): Observable<BuildStep> {
+    return this.httpService.postRequest<BuildStep>(`${this.routePrefix}`, buildStep);
+  }
+
+  removeBuildStep(buildStep: BuildStep){
+    return this.httpService.deleteRequest(`${this.routePrefix}/${buildStep.id}`);
+  }
+}
