5ea498408116e019b0b27c32eedbd3b9fc4b0396
rusgumeniuk <rus.gumeniuk@gmail.com>
2020-08-20 20:04:49 +0300

    Implement GetEmptyBuildSteps and GetBuildStepsByProjectId
    need to change Delete

--- backend/buildeR.BLL/Services/BuildStepService.cs
+++ backend/buildeR.BLL/Services/BuildStepService.cs
@@ -1,19 +1,27 @@
 ﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+
 using buildeR.BLL.Exceptions;
 using buildeR.BLL.Interfaces;
 using buildeR.BLL.Services.Abstract;
+using buildeR.Common.DTO.BuildPlugin;
 using buildeR.Common.DTO.BuildStep;
+using buildeR.Common.DTO.PluginCommand;
 using buildeR.DAL.Context;
 using buildeR.DAL.Entities;
+
+using Microsoft.EntityFrameworkCore;
+
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace buildeR.BLL.Services
 {
     public class BuildStepService : BaseCrudService<BuildStep, BuildStepDTO, NewBuildStepDTO>, IBuildStepService
     {
-        public BuildStepService(BuilderContext context, IMapper mapper) : base(context, mapper) {}
+        public BuildStepService(BuilderContext context, IMapper mapper) : base(context, mapper) { }
         public async Task<BuildStepDTO> GetBuildStepById(int id)
         {
             var buildStep = await base.GetAsync(id);
@@ -51,7 +59,36 @@ namespace buildeR.BLL.Services
             {
                 throw new NotFoundException(nameof(BuildStep), id);
             }
+            //TODO: reorder other build steps of this project (change indexes)
             await base.RemoveAsync(id);
+        }
+
+        public async Task<IEnumerable<EmptyBuildStepDTO>> GetEmptyBuildSteps()
+        {
+            var buildPlugins = await Context.Set<BuildPlugin>().ToListAsync();
+            var pluginCommands = await Context.Set<PluginCommand>().ToListAsync();
+            return buildPlugins
+                .Join(pluginCommands,
+                    buildPlugin => buildPlugin.Id,
+                    pluginCommand => pluginCommand.PluginId,
+                    (buildPlugin, pluginCommand) => new EmptyBuildStepDTO()
+                    {
+                        BuildStepName = $"{buildPlugin.PluginName}: {pluginCommand.Name}",
+                        BuildPluginId = buildPlugin.Id,
+                        BuildPlugin = Mapper.Map<BuildPluginDTO>(buildPlugin),
+                        PluginCommand = Mapper.Map<PluginCommandDTO>(pluginCommand),
+                        PluginCommandId = pluginCommand.Id
+                    }
+                );
+        }
+
+        public async Task<IEnumerable<BuildStepDTO>> GetBuildStepsByProjectId(int projectId)
+        {
+            return await Context
+                .BuildSteps
+                .Where(buildStep => buildStep.ProjectId == projectId)
+                .ProjectTo<BuildStepDTO>(Mapper.ConfigurationProvider)
+                .ToListAsync();
         }
     }
 }
