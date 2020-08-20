d2d616024c21c489db62dcae00f46b1f2c9e1bc6
MaksymFilypchuk <mfilypchuk@gmail.com>
2020-08-20 15:49:56 +0300

    fix add  resolve UserResolverService

--- frontend/src/app/modules/user/user-routing.module.ts
+++ frontend/src/app/modules/user/user-routing.module.ts
@@ -25,6 +25,9 @@ import { NotificationSettingComponent } from './notification-setting/notificatio
     {
       path: 'notificationsettings',
       component: NotificationSettingComponent,
+      resolve: {
+        user: UserResolverService
+      }
     },
   ]
   }])],
