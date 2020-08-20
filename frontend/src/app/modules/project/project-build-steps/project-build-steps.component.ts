1aa67ff377977bea5b0e3faf6fe2d89e36750066
rusgumeniuk <rus.gumeniuk@gmail.com>
2020-08-20 20:06:16 +0300

    Set up ProjectBuildStepsComponent to work

--- frontend/src/app/modules/project/project-build-steps/project-build-steps.component.ts
+++ frontend/src/app/modules/project/project-build-steps/project-build-steps.component.ts
@@ -1,15 +1,151 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Project } from '@shared/models/project/project';
+import { ToastrNotificationsService } from '@core/services/toastr-notifications.service';
+import { ActivatedRoute } from '@angular/router';
+import { ProjectService } from '@core/services/project.service';
+import { BuildStepService } from '@core/services/build-step.service';
+import { BaseComponent } from '@core/components/base/base.component';
+import { takeUntil } from 'rxjs/operators';
+import { BuildStep } from '@shared/models/build-step';
+import { EmptyBuildStep } from '@shared/models/empty-build-step';
+
 
 @Component({
   selector: 'app-project-build-steps',
   templateUrl: './project-build-steps.component.html',
   styleUrls: ['./project-build-steps.component.sass']
 })
-export class ProjectBuildStepsComponent implements OnInit {
+export class ProjectBuildStepsComponent extends BaseComponent implements OnInit, OnDestroy {
+  projectId: number;
+  project: Project = {} as Project;
+  buildSteps: BuildStep[];
+  emptyBuildSteps: EmptyBuildStep[];
+  isLoading = true;
+  isAdding = false;
+  newBuildStep: EmptyBuildStep = {} as EmptyBuildStep;
+  workDir: string;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private projectService: ProjectService,
+    private buildStepService: BuildStepService,
+    private toastrService: ToastrNotificationsService,
+    private route: ActivatedRoute
+  ) {
+    super();
+    route.parent.params.subscribe((params) => this.projectId = params.projectId);
   }
 
+
+  ngOnDestroy(): void {
+    super.ngOnDestroy();
+  }
+
+  ngOnInit(): void {
+    this.project = this.route.snapshot.data.project as Project;
+    this.getProject(this.projectId);
+    this.getProjectBuildSteps(this.projectId);
+    this.getEmptyBuildSteps();
+    this.isLoading = false;
+  }
+
+  getProject(projectId: number) {
+    this.isLoading = true;
+    this.projectService
+      .getProjectById(projectId)
+      .subscribe(
+        (data) => {
+          this.project = data;
+        },
+        (error) => {
+          this.toastrService.showError(error.message, error.name);
+        }
+      );
+  }
+
+  getEmptyBuildSteps() {
+    this.isLoading = true;
+    this.buildStepService
+      .getEmptyBuildSteps()
+      .pipe(takeUntil(this.unsubscribe$))
+      .subscribe(
+        (resp) => {
+          this.emptyBuildSteps = resp.body;
+        },
+        (error) => {
+          this.toastrService.showError(error);
+        }
+      );
+  }
+
+  getProjectBuildSteps(projectId: number) {
+    this.isLoading = true;
+    this.buildStepService
+      .getBuildStepsByProject(projectId)
+      .pipe(takeUntil(this.unsubscribe$))
+      .subscribe(
+        (resp) => {
+          this.buildSteps = resp.body;
+        },
+        (error) => {
+          this.toastrService.showError(error);
+        }
+      );
+  }
+
+  addBuildStep() {
+    this.isAdding = true;
+    this.newBuildStep = this.emptyBuildSteps[0];
+  }
+
+  cancelBuildStep() {
+    this.newBuildStep = null;
+    this.isAdding = false;
+    this.workDir = null;
+  }
+
+  saveBuildStep() {
+    const buildStep = {} as BuildStep;
+
+    buildStep.pluginCommand = this.newBuildStep.pluginCommand;
+    buildStep.index = this.buildSteps.length + 1;
+    buildStep.buildStepName = this.newBuildStep.buildStepName;
+    buildStep.pluginCommandId = this.newBuildStep.pluginCommand.id;
+    buildStep.projectId = this.projectId;
+    buildStep.workDirectory = this.workDir;
+
+    this.newBuildStep = null;
+    this.isAdding = false;
+
+    this.isLoading = true;
+    this.buildStepService
+      .createBuildStep(buildStep)
+      .pipe(takeUntil(this.unsubscribe$))
+      .subscribe(
+        (resp) => {
+          this.isLoading = false;
+          this.buildSteps.push(resp);
+        },
+        (error) => {
+          this.isLoading = false;
+          this.toastrService.showError(error);
+        }
+      );
+  }
+
+  removeBuildStep(buildStep: BuildStep) {
+    this.isLoading = true;
+    this.buildStepService
+      .removeBuildStep(buildStep)
+      .pipe(takeUntil(this.unsubscribe$))
+      .subscribe(
+        (resp) => {
+          this.isLoading = false;
+          this.buildSteps = this.buildSteps.filter(step => buildStep.id !== step.id);
+        },
+        (error) => {
+          this.isLoading = false;
+          this.toastrService.showError(error);
+        }
+      );
+  }
 }
