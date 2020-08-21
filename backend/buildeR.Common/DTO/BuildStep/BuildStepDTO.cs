e4a507f928d10524c7500f817adf77a044ca1ee1
jyuart <yura.palayda@gmail.com>
2020-08-21 11:31:11 +0300

    Change buildstepDTO

--- backend/buildeR.Common/DTO/BuildStep/BuildStepDTO.cs
+++ backend/buildeR.Common/DTO/BuildStep/BuildStepDTO.cs
@@ -1,5 +1,7 @@
 ﻿using buildeR.Common.DTO.BuildPlugin;
 using buildeR.Common.DTO.BuildPluginParameter;
+using buildeR.Common.DTO.PluginCommand;
+using buildeR.Common.Enums;
 
 using System.Collections.Generic;
 
@@ -8,12 +10,12 @@ namespace buildeR.Common.DTO.BuildStep
     public sealed class BuildStepDTO
     {
         public int Id { get; set; }
-        public string BuildStepName { get; set; }
-        public int BuildPluginId { get; set; }
-        public int LoggingVerbosity { get; set; }
-        public int ProjectId { get; set; }
-
+        public string Name { get; set; }
+        public string WorkDirectory { get; set; }
+        public PluginCommandDTO PluginCommand { get; set; }
         public BuildPluginDTO BuildPlugin { get; set; }
-        public ICollection<BuildPluginParameterDTO> BuildPluginParameters { get; set; }
+
+        public LoggingVerbosity LoggingVerbosity { get; set; }
+        public IEnumerable<BuildPluginParameterDTO> Parameters { get; set; }
     }
 }
