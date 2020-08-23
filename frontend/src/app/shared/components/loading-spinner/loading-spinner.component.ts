edac0237c42241e120ce9939003e5e1ae94786a9
rusgumeniuk <rus.gumeniuk@gmail.com>
2020-08-23 21:10:38 +0300

    Generate LoadingSpinnerComponent

---
+++ frontend/src/app/shared/components/loading-spinner/loading-spinner.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit, Input, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+
+@Component({
+  selector: 'app-loading-spinner',
+  templateUrl: './loading-spinner.component.html',
+  styleUrls: ['./loading-spinner.component.sass']
+})
+export class LoadingSpinnerComponent implements OnInit, AfterViewInit {
+
+  @Input()
+  size = '20px';
+
+  @ViewChild('spinner')
+  spinner: ElementRef;
+
+  constructor() { }
+
+  ngOnInit(): void { }
+
+  ngAfterViewInit() {
+    (this.spinner.nativeElement as HTMLDivElement).style.fontSize = this.size;
+  }
+}
