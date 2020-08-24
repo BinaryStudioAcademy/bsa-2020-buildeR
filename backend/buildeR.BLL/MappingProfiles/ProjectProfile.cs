139fbc6ae44261362cd6a9d8b9d8a7df52cca3a3
yermolenko-d <shamanovdimasig@gmail.com>
2020-08-24 09:51:19 +0000

    fixed save project issue

--- backend/buildeR.BLL/MappingProfiles/ProjectProfile.cs
+++ backend/buildeR.BLL/MappingProfiles/ProjectProfile.cs
@@ -4,6 +4,7 @@ using buildeR.Common.DTO.Project;
 using buildeR.DAL.Entities;
 
 using System.Linq;
+using System.Security.Cryptography;
 
 namespace buildeR.BLL.MappingProfiles
 {
@@ -20,7 +21,31 @@ namespace buildeR.BLL.MappingProfiles
                             .OrderByDescending(prj => prj.BuildAt)
                             .FirstOrDefault()));
 
-            CreateMap<ProjectDTO, Project>();
+            CreateMap<ProjectDTO, Project>()
+                .ForMember(dest => dest.Owner,
+                    src => src.MapFrom(project => project
+                    .Owner))
+                .ForMember(dest => dest._Repository,
+                   src => src
+                   .MapFrom(project => project
+                   ._Repository))
+                .ForMember(dest => dest.ProjectGroups,
+                src => src.Ignore())
+                .ForMember(dest => dest.BuildHistories,
+                   src => src
+                   .MapFrom(project => project
+                   .BuildHistories))
+                .ForMember(dest => dest.BuildSteps,
+                   src => src
+                   .MapFrom(project => project
+                   .BuildSteps))
+                .ForMember(dest => dest.ProjectTriggers,
+                   src => src
+                   .MapFrom(project => project
+                   .ProjectTriggers));
+
+
+
             CreateMap<NewProjectDTO, Project>();
         }
     }
