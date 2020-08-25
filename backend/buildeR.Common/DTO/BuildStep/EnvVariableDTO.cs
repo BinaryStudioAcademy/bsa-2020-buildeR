e5dd34f10fba60360e756f9371f9f1a15ee61886
rusgumeniuk <rus.gumeniuk@gmail.com>
2020-08-25 19:50:39 +0300

    Rename EnvVariable to EnvVariableDTO

---
+++ backend/buildeR.Common/DTO/BuildStep/EnvVariableDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace buildeR.Common.DTO.BuildStep
+{
+    public class EnvVariableDTO
+    {
+        public int Id { get; set; }
+        public string Key { get; set; }
+        public string Value { get; set; }
+    }
+}
