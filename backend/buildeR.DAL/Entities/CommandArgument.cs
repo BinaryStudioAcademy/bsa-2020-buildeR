6ee2e12c4802fc8e6f64d78961afb28a1f3794fb
rusgumeniuk <rus.gumeniuk@gmail.com>
2020-08-25 19:51:40 +0300

    Create entities EnvVariable and CommandArgument

---
+++ backend/buildeR.DAL/Entities/CommandArgument.cs
@@ -0,0 +1,13 @@
+﻿using buildeR.DAL.Entities.Common;
+
+namespace buildeR.DAL.Entities
+{
+    public class CommandArgument : Entity
+    {
+        public string Key { get; set; }
+        public string Value { get; set; }
+
+        public int BuildStepId { get; set; }
+        public BuildStep BuildStep { get; set; }
+    }
+}
