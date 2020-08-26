2ab9e85dec249080176785066a928142b2b1f32b
RomanKuk <kuk2roman@gmail.com>
2020-08-26 13:02:31 +0300

    update loading spiner component

--- frontend/src/app/shared/components/loading-spinner/loading-spinner.component.ts
+++ frontend/src/app/shared/components/loading-spinner/loading-spinner.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit, Input, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-loading-spinner',
   templateUrl: './loading-spinner.component.html',
   styleUrls: ['./loading-spinner.component.sass']
 })
-export class LoadingSpinnerComponent implements OnInit, AfterViewInit {
+export class LoadingSpinnerComponent implements OnInit {
 
   @Input() size = '20px';
   @Input() top = '30%';
@@ -20,11 +20,4 @@ export class LoadingSpinnerComponent implements OnInit, AfterViewInit {
 
   ngOnInit(): void { }
 
-  ngAfterViewInit() {
-    (this.spinner.nativeElement as HTMLDivElement).style.fontSize = this.size;
-    (this.spinner.nativeElement as HTMLDivElement).style.top = this.top;
-    (this.spinner.nativeElement as HTMLDivElement).style.left = this.left;
-    (this.spinner.nativeElement as HTMLDivElement).style.position = this.position;
-    (this.spinner.nativeElement as HTMLDivElement).style.margin = this.margin;
-  }
 }
