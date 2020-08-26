4361036b7da5e8aff8d1c35ce27d0116bca97794
yermolenko-d <shamanovdimasig@gmail.com>
2020-08-26 10:50:23 +0000

    added first part of frontend

--- frontend/src/app/modules/user/insights/insights.component.ts
+++ frontend/src/app/modules/user/insights/insights.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from '@core/services/authentication.service';
 import { User } from '@shared/models/user/user';
+import { TabRoute } from '@shared/models/tabs/tab-route';
 
 @Component({
   templateUrl: './insights.component.html',
@@ -9,6 +10,11 @@ import { User } from '@shared/models/user/user';
 export class InsightsComponent implements OnInit {
   user: User = this.authService.getCurrentUser();
   now: Date = new Date(Date.now());
+  tabRoutes: TabRoute[] = [
+    { name: 'Week', route: '' },
+    { name: 'Month', route: '' },
+  ];
+
   constructor(private authService: AuthenticationService) { }
 
   ngOnInit(): void {
