d7a41abd6605fe658123730fc0b8937066fdc616
Anton Matsyshyn <tosha.matsyshyn@gmail.com>
2020-08-26 13:01:52 +0300

    Change GithubClient

--- backend/buildeR.BLL/Interfaces/IGithubClient.cs
+++ backend/buildeR.BLL/Interfaces/IGithubClient.cs
@@ -6,11 +6,10 @@ namespace buildeR.BLL.Interfaces
 {
     public interface IGithubClient
     {
-        Task<GithubUser> GetUserFromToken(string providerToken);
-        Task<IEnumerable<GithubRepository>> GetUserRepositories(string providerToken);
-        Task<IEnumerable<GithubBranch>> GetRepositoryBranches(string repositoryOwner, string repositoryName, string providerToken);
-        Task<bool> CheckIfRepositoryAccessable(string repoOwner, string repoName);
-        Task<IEnumerable<GithubBranch>> GetRepositoryBranches(string repositoryName, string providerToken);
-        Task CreateWebhook(string repositoryName, string callback, string providerToken);
+        Task<GithubUser> GetUserFromCredentials(string username, string password);
+        Task<IEnumerable<GithubRepository>> GetUserRepositories(string username, string password);
+        Task<IEnumerable<GithubBranch>> GetRepositoryBranches(string repositoryName, string username, string password);
+        Task<bool> CheckIfRepositoryAccessable(string repoOwner, string repoName, string username = null, string password = null);
+        Task CreateWebhook(string repositoryName, string callback, string username, string password);
     }
 }
