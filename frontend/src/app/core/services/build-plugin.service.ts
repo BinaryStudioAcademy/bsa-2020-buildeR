ea130e3641f016f8f956d82d5aedbad4e25787d9
rusgumeniuk <rus.gumeniuk@gmail.com>
2020-08-26 10:07:29 +0300

    Create BuildPluginService with function getVersionsOfBuildPlugin

---
+++ frontend/src/app/core/services/build-plugin.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpService } from '../../core/services/http.service';
+import { Observable } from 'rxjs';
+import { HttpResponse } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BuildPluginService {
+
+  public routePrefix = '/buildPlugins';
+
+  constructor(private httpService: HttpService) { }
+
+  getVersionsOfBuildPlugin(buildPluginName: string, partOfVersionTerm: string): Observable<HttpResponse<string[]>> {
+    return this.httpService.getFullRequest<string[]>(`${this.routePrefix}/${buildPluginName}/versions/${partOfVersionTerm}`);
+  }
+}
