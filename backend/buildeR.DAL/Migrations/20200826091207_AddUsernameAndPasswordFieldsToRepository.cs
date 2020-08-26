622dcb2841590e8c72b45309329c356e0604ca29
Anton Matsyshyn <tosha.matsyshyn@gmail.com>
2020-08-26 12:29:17 +0300

    Add migration for new repository fields

---
+++ backend/buildeR.DAL/Migrations/20200826091207_AddUsernameAndPasswordFieldsToRepository.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace buildeR.DAL.Migrations
+{
+    public partial class AddUsernameAndPasswordFieldsToRepository : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Password",
+                table: "Repositories",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Username",
+                table: "Repositories",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Password",
+                table: "Repositories");
+
+            migrationBuilder.DropColumn(
+                name: "Username",
+                table: "Repositories");
+        }
+    }
+}
