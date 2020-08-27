038f0b8562aa019baeb71f3e6371372f7d922d10
Anton Matsyshyn <tosha.matsyshyn@gmail.com>
2020-08-27 17:07:15 +0300

    Add lowdash to repositorylink allowed characters

--- frontend/src/app/modules/project/project-create/project-create.component.ts
+++ frontend/src/app/modules/project/project-create/project-create.component.ts
@@ -171,7 +171,7 @@ export class ProjectCreateComponent implements OnInit {
     this.projectForm.addControl('repositoryURL', new FormControl(this.newProject.repository.url,
       [
         Validators.required,
-        Validators.pattern(`https:\/\/github\.com\/[A-Za-z0-9.-]+\/[A-Za-z0-9.-]+`)
+        Validators.pattern(`https:\/\/github\.com\/[A-Za-z0-9-]+\/[A-Za-z0-9_.-]+`)
       ],
       [
         repoUrlAsyncValidator(this.syncService, this.user),
