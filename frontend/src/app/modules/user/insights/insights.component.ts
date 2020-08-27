b3902741757f4df497e02b8d6958f2ca554b4e5f
yermolenko-d <shamanovdimasig@gmail.com>
2020-08-27 14:25:22 +0000

    showData fixes

--- frontend/src/app/modules/user/insights/insights.component.ts
+++ frontend/src/app/modules/user/insights/insights.component.ts
@@ -16,7 +16,10 @@ export class InsightsComponent implements OnInit {
   totalDuration = 0;
   buildSuccessRate = 0;
   tab = 0;
+
   dataForBuilds;
+  dataForDuration;
+  dataForSuccess;
 
   weekMonth = [
     { name: 'Week' },
@@ -37,7 +40,17 @@ export class InsightsComponent implements OnInit {
       buildStatus: 0,
       commitHash: null,
       duration: 10
+    }, {
+      id : 2,
+      number: 2,
+      performer: this.user,
+      branchHash: null,
+      buildAt: new Date(2020, 7, 25),
+      buildStatus: 2,
+      commitHash: null,
+      duration: 19
     });
+    console.log(this.user.builds);
     this.totalBuilds = this.totalBuildsCount();
     this.totalDuration = this.user.builds.map(this.duration).reduce(this.sum);
     this.buildSuccessRate = this.buildSucceedCount();
@@ -69,8 +82,10 @@ export class InsightsComponent implements OnInit {
   showWeek(){
     const diff = this.diffDates(this.now, this.user.createdAt);
     if (diff <= 7){
-    console.log(this.createDataForBuildsChart(this.user.createdAt, diff));
-    this.dataForBuilds = this.createDataForBuildsChart(this.user.createdAt, diff);
+      console.log(this.createDataForBuildsChart(this.user.createdAt, diff));
+      this.dataForBuilds = this.createDataForBuildsChart(this.user.createdAt, diff);
+      this.dataForDuration = this.createDataForDuration(this.user.createdAt, diff);
+      this.dataForSuccess = this.createDataForSuccess(this.user.createdAt, diff);
     }
   }
 
@@ -81,25 +96,75 @@ export class InsightsComponent implements OnInit {
       const newDay =  new Date(startDate);
       newDay.setDate(newDay.getDate() + index);
       const value = this.countBuildsInDay(newDay);
-      const name = newDay.getDay();
+      const name = this.beautifyDate(newDay);
+      console.log(name);
       result.push({name, value});
     }
     return [{name: 'builds', series: result}];
   }
 
+  createDataForDuration(startDate: Date, days: number){
+    const result = new Array();
+    startDate = new Date(startDate);
+    for (let index = 0; index <= days; index++) {
+      const newDay =  new Date(startDate);
+      newDay.setDate(newDay.getDate() + index);
+      const value = this.countDurationInDay(newDay);
+      const name = this.beautifyDate(newDay);
+      result.push({name, value});
+    }
+    return [{name: 'minutes', series: result}];
+  }
+
+  createDataForSuccess(startDate: Date, days: number){
+    const result = new Array();
+    startDate = new Date(startDate);
+    for (let index = 0; index <= days; index++) {
+      const newDay =  new Date(startDate);
+      newDay.setDate(newDay.getDate() + index);
+      const value = this.CountSucceedBuildsInDay(newDay);
+      const name = this.beautifyDate(newDay);
+      result.push({name, value});
+    }
+    return [{name: 'Succeed', series: result}];
+  }
+
+  beautifyDate(newDay: Date){
+    newDay = new Date(newDay);
+    if (newDay.getMonth() + 1 < 10){
+      return newDay.getDate() + '.0' + (newDay.getMonth() + 1);
+    }
+    return newDay.getDay() + '.' + (newDay.getMonth() + 1);
+  }
+
   countBuildsInDay(day: Date){
     day = new Date(day);
+    console.log(day);
     return this.user.builds.filter(x => x.buildAt.getDay() === day.getDay()).length;
   }
 
+  CountSucceedBuildsInDay(day: Date){
+    day = new Date(day);
+    return this.user.builds.filter(x => x.buildAt.getDay() === day.getDay()
+    && x.buildStatus === BuildStatus.Success).length;
+  }
+
+  countDurationInDay(day: Date){
+    day = new Date(day);
+    const builds = this.user.builds.filter(x => x.buildAt.getDay() === day.getDay());
+    console.log(builds);
+    if (builds.length > 0){
+      console.log(builds.map(this.duration).reduce(this.sum));
+      return builds.map(this.duration).reduce(this.sum);
+    }
+    return 0;
+  }
 
   diffDates(dateOne: Date, dateTwo: Date): number {
     dateOne = new Date(dateOne);
     dateTwo = new Date(dateTwo);
     return Math.floor((dateOne.getTime() - dateTwo.getTime()) / (60 * 60 * 24 * 1000));
 }
-
-
 
   duration(history: BuildHistory){
     return history.duration;
