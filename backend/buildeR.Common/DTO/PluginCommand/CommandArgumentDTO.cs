7a4c3eb73712b7f2f012be1d0b3fbbc898e814c9
jyuart <yura.palayda@gmail.com>
2020-08-27 18:53:48 +0300

    Change buildstep and command argument models

--- backend/buildeR.Common/DTO/PluginCommand/CommandArgumentDTO.cs
+++ backend/buildeR.Common/DTO/PluginCommand/CommandArgumentDTO.cs
@@ -1,9 +1,13 @@
-﻿namespace buildeR.Common.DTO
+﻿using buildeR.Common.DTO.BuildStep;
+
+namespace buildeR.Common.DTO
 {
     public class CommandArgumentDTO
     {
         public int Id { get; set; }
         public string Key { get; set; }
         public string Value { get; set; }
+        public int BuildStepId { get; set; }
+        public BuildStepDTO BuildStep { get; set; }
     }
 }
