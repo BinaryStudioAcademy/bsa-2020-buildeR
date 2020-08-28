c900f69fae5c245d79c49a6eb1fd266da9c8167d
Zhenia Borodaikevych <borodaykevich@gmail.com>
2020-08-28 09:17:13 +0300

    fixed build status pipe missed pending value

--- frontend/src/app/shared/pipes/build-status.pipe.ts
+++ frontend/src/app/shared/pipes/build-status.pipe.ts
@@ -3,10 +3,11 @@ import { BuildStatus } from '../models/build-status';
 
 @Pipe({
   name: 'buildStatus',
+  // pure: false
 })
 export class BuildStatusPipe implements PipeTransform {
   transform(value: number): string {
-    if (value <= BuildStatus.Canceled) {
+    if (value <= BuildStatus.Pending) {
       return BuildStatus[value];
     }
     if (value == BuildStatus.InProgress) {
