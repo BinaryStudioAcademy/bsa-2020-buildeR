5e932e95e6a0bd779fb9d60448620e0806c78a5c
jyuart <yura.palayda@gmail.com>
2020-08-30 15:34:32 +0300

    Add custom command plugin and custom command seed data to db

--- backend/buildeR.DAL/Migrations/20200830123252_Add custom command plugin and custom command to db.Designer.cs
+++ backend/buildeR.DAL/Migrations/20200830123252_Add custom command plugin and custom command to db.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using buildeR.DAL.Context;
 
 namespace buildeR.DAL.Migrations
 {
     [DbContext(typeof(BuilderContext))]
-    partial class BuilderContextModelSnapshot : ModelSnapshot
+    [Migration("20200830123252_Add custom command plugin and custom command to db")]
+    partial class Addcustomcommandpluginandcustomcommandtodb
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
