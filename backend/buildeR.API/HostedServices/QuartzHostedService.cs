1815ca32a5f170d0159b386fdb07d804e77c50ba
Volodymyr <volodymyr_bilyk@outlook.com>
2020-08-31 12:09:24 +0300

    Fixed quartz

---
+++ backend/buildeR.API/HostedServices/QuartzHostedService.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Hosting;
+using Quartz;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace buildeR.API.HostedServices
+{
+    public class QuartzHostedService : IHostedService
+    {
+        private readonly ISchedulerFactory _schedulerFactory;
+        public IScheduler Scheduler { get; set; }
+
+
+        public QuartzHostedService(ISchedulerFactory schedulerFactory)
+        {
+            _schedulerFactory = schedulerFactory;
+        }
+
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            Scheduler = await _schedulerFactory.GetScheduler(cancellationToken);
+            await Scheduler.Start(cancellationToken);
+        }
+
+        public async Task StopAsync(CancellationToken cancellationToken)
+        {
+            await Scheduler?.Shutdown(cancellationToken);
+        }
+    }
+}
