2afa88f76d6823be5f7e93da66c8d799964c94bf
LizaPazhukova <57843676+LizaPazhukova@users.noreply.github.com>
2020-09-01 16:05:22 +0300

    send letter when invite to the group

--- backend/buildeR.BLL/Services/TeamMemberService.cs
+++ backend/buildeR.BLL/Services/TeamMemberService.cs
@@ -5,6 +5,7 @@ using buildeR.BLL.Services.Abstract;
 using buildeR.Common.DTO.TeamMember;
 using buildeR.DAL.Context;
 using buildeR.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
@@ -13,7 +14,22 @@ namespace buildeR.BLL.Services
 {
     public class TeamMemberService : BaseCrudService<TeamMember, TeamMemberDTO, NewTeamMemberDTO>, ITeamMemberService
     {
-        public TeamMemberService(BuilderContext context, IMapper mapper) : base(context, mapper) { }
+        private readonly BuilderContext _context;
+        private readonly IEmailService _emailService;
+        private readonly IEmailBuilder _emailBuilder;
+        private readonly IUserService _userService;
+        public TeamMemberService(
+            BuilderContext context, 
+            IMapper mapper, 
+            IUserService userService, 
+            IEmailBuilder emailBuilder,
+            IEmailService emailService) : base(context, mapper) 
+        {
+            _context = context;
+            _userService = userService;
+            _emailBuilder = emailBuilder;
+            _emailService = emailService;
+        }
         public async Task<TeamMemberDTO> Create(NewTeamMemberDTO teamMember)
         {
             if (teamMember == null)
@@ -21,7 +37,17 @@ namespace buildeR.BLL.Services
                 throw new ArgumentNullException();
             }
             teamMember.JoinedDate = DateTime.Now;
+
+            var user = await _context.Users.Include(u => u.NotificationSetting).FirstOrDefaultAsync(u => u.Id == teamMember.UserId);
+            if (user.NotificationSetting.EnableEmail)
+            {
+                var emailModel = _emailBuilder.GetInviteGroupLetter(user.Email, user.FirstName);
+                await _emailService.SendEmailAsync(new List<string> { emailModel.Email }, emailModel.Subject, emailModel.Title, emailModel.Body);
+            }
+
             return await base.AddAsync(teamMember);
+            
+
         }
 
         public async Task Delete(int id)
