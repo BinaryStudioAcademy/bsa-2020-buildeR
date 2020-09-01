2da83e1793b1673bbca6c3112492bdfc880bddac
Anton Matsyshyn <tosha.matsyshyn@gmail.com>
2020-09-01 20:18:43 +0300

    Add ProjectRemoteTriggerDTO

---
+++ backend/buildeR.Common/DTO/RemoteTrigger/ProjectRemoteTriggerDTO.cs
@@ -0,0 +1,11 @@
+﻿using buildeR.Common.Enums;
+
+namespace buildeR.Common.DTO.RemoteTrigger
+{
+    public class ProjectRemoteTriggerDTO
+    {
+        public RemoteTriggerType Type { get; set; }
+        public string Branch { get; set; }
+        public int ProjectId { get; set; }
+    }
+}
