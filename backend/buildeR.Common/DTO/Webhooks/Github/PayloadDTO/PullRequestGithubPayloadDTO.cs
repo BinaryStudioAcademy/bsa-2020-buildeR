d00e8c47c045dcc9e89816d4d4f4bf57dd179570
Anton Matsyshyn <tosha.matsyshyn@gmail.com>
2020-09-01 17:36:49 +0300

    Add pull request payload models

---
+++ backend/buildeR.Common/DTO/Webhooks/Github/PayloadDTO/PullRequestGithubPayloadDTO.cs
@@ -0,0 +1,10 @@
+﻿using buildeR.Common.DTO.Webhooks.Github.Shared;
+
+namespace buildeR.Common.DTO.Webhooks.Github.PayloadDTO
+{
+    public class PullRequestGithubPayloadDTO
+    {
+        public string Action { get; set; }
+        public PullRequestGithubDTO Pull_Request { get; set; }
+    }
+}
---
+++ backend/buildeR.Common/DTO/Webhooks/Github/Shared/BranchGithubDTO.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace buildeR.Common.DTO.Webhooks.Github.Shared
+{
+    public class BranchGithubDTO
+    {
+        public string Ref { get; set; }
+    }
+}
