4bbd537d04c3b021bbd5c970ba6779b57eae2cc8
Anton Matsyshyn <tosha.matsyshyn@gmail.com>
2020-09-02 11:05:07 +0300

    Add project remote dto

---
+++ backend/buildeR.BLL/MappingProfiles/ProjectRemoteTriggerProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using buildeR.Common.DTO.ProjectRemoteTrigger;
+using buildeR.DAL.Entities;
+
+namespace buildeR.BLL.MappingProfiles
+{
+    public class ProjectRemoteTriggerProfile: Profile
+    {
+        public ProjectRemoteTriggerProfile()
+        {
+            CreateMap<NewProjectRemoteTriggerDTO, ProjectRemoteTrigger>();
+            CreateMap<ProjectRemoteTrigger, ProjectRemoteTriggerDTO>();
+        }
+    }
+}
--- backend/buildeR.Common/DTO/Webhooks/Github/PayloadDTO/PushGithubPayloadDTO.cs
+++ backend/buildeR.Common/DTO/Webhooks/Github/PayloadDTO/PushGithubPayloadDTO.cs
@@ -1,11 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using buildeR.Common.DTO.Webhooks.Github.Shared;
 
 namespace buildeR.Common.DTO.Webhooks.Github.PayloadDTO
 {
     public class PushGithubPayloadDTO
     {
         public string Ref { get; set; }
+        public UserGithubDTO Sender { get; set; }
     }
 }
