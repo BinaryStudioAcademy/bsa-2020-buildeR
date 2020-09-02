fb864907e6a63cba13ff55d8369578b9b5ca6f41
RomanKuk <kuk2roman@gmail.com>
2020-09-02 18:09:48 +0300

    clean code

--- frontend/src/app/core/resolvers/user.resolver.ts
+++ frontend/src/app/core/resolvers/user.resolver.ts
@@ -19,7 +19,7 @@ export class UserResolverService implements Resolve<User>{
       userId = this.authService.getCurrentUser().id;
     }
     return this.userService.getUserByIdRequest(userId).pipe(tap((resp) => {
-      return resp;
+      return resp ?? EMPTY;
     }), catchError(() => {
       this.router.navigateByUrl('/portal/**', { skipLocationChange: true });
       return EMPTY;
