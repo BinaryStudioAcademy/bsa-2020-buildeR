c1512f20308ae164f8921e7f075fe2f5b4acb9aa
jyuart <yura.palayda@gmail.com>
2020-09-03 09:03:18 +0300

    Add method for getting all methods for specific plugin

--- backend/buildeR.BLL/Interfaces/IPluginCommandService.cs
+++ backend/buildeR.BLL/Interfaces/IPluginCommandService.cs
@@ -1,10 +1,18 @@
-﻿using System;
+﻿using buildeR.Common.DTO.PluginCommand;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace buildeR.BLL.Interfaces
 {
-    class IPluginCommandService
+    public interface IPluginCommandService
     {
+        Task<PluginCommandDTO> GetCommandById(int id);
+        Task<IEnumerable<PluginCommandDTO>> GetAll();
+        Task<IEnumerable<PluginCommandDTO>> GetCommandsByPluginId();
+        Task<PluginCommandDTO> Create(NewPluginCommandDTO pluginCommand);
+        Task Update(PluginCommandDTO pluginCommand);
+        Task Delete(int id);
     }
 }
