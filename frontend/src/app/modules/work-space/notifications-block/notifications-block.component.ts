dee7a643acd23c5caf5d4ff4e50760fd20b1cbe2
Zhenia Borodaikevych <borodaykevich@gmail.com>
2020-09-04 02:02:24 +0300

    added navigation to details of notification's item

--- frontend/src/app/modules/work-space/notifications-block/notifications-block.component.ts
+++ frontend/src/app/modules/work-space/notifications-block/notifications-block.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Notification } from '../../../shared/models/notification';
 import { NotificationType } from '../../../shared/models/notification-type';
 import { NotificationsService } from '@core/services/notifications.service';
+import { Router, ActivatedRoute } from '@angular/router';
+import { BuildHistoryService } from '@core/services/build-history.service';
 
 @Component({
   selector: 'app-notifications-block',
@@ -17,20 +19,23 @@ export class NotificationsBlockComponent implements OnInit {
   @Output() counterNotifications = new EventEmitter<number>();
   @Output() toggleNotifications = new EventEmitter<void>();
 
-  constructor(private notificationService: NotificationsService) {}
+  constructor(
+    private notificationService: NotificationsService,
+    private router: Router,
+    private buildHistoryService: BuildHistoryService
+  ) {}
 
   ngOnInit(): void {
-    this.notificationService.listen()
-    .subscribe((notification) => {
+    this.notificationService.listen().subscribe((notification) => {
       this.notifications.push(notification);
-      this.onChanging("adding", notification);
+      this.onChanging('adding', notification);
     });
   }
 
   clearOne(notification: Notification) {
     this.notificationService.markAsRead(notification.id);
     notification.isRead = true;
-    this.onChanging("reading", notification);
+    this.onChanging('reading', notification);
   }
 
   clearAll() {
@@ -43,14 +48,29 @@ export class NotificationsBlockComponent implements OnInit {
     this.toggleNotifications.emit();
   }
 
-  onChanging(type: "adding" | "reading", notification: Notification) {
-    if (type == "adding") {
+  onChanging(type: 'adding' | 'reading', notification: Notification) {
+    if (type == 'adding') {
       if (this.isShowingRead || !notification.isRead) {
         this.counterNotifications.emit(1);
       }
     } else {
       if (!this.isShowingRead) {
         this.counterNotifications.emit(-1);
+      }
+    }
+  }
+
+  navigateToItem(notification: Notification) {
+    if (notification.itemId) {
+      switch (notification.type) {
+        case NotificationType.BuildCanceled:
+        case NotificationType.BuildErrored:
+        case NotificationType.BuildFailed:
+        case NotificationType.BuildSucceeded: {
+          this.buildHistoryService.getBuildHistory(notification.itemId).subscribe(
+            bh => this.router.navigate(["portal", "projects", bh.projectId, "history", notification.itemId])
+          );
+        }
       }
     }
   }
