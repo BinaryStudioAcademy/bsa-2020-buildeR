0e87f4c27e4f18f6d94afac2905bac9d18a94cec
nazar-kotsun <kotsun.nazar69@gmail.com>
2020-09-06 18:42:15 +0300

    Add a new DTO

---
+++ backend/buildeR.Common/DTO/UserLetterAnswerDTO.cs
@@ -0,0 +1,12 @@
+namespace buildeR.Common.DTO
+{
+    public class UserLetterAnswerDTO
+    {
+        public string UserName { get; set; }
+        public string UserEmail { get; set; }
+        public string Subject { get; set; }
+        public string Description { get; set; }
+        public bool IsRespond { get; set; }
+        public string Answer { get; set; }
+    }
+}
