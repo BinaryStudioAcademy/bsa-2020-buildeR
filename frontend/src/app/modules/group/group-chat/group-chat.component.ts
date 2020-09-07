5ba979cdcc3e34b45dbd928b22c89c7f0b2d8b2c
Yermolenko Dmitri <41588734+yermolenko-d@users.noreply.github.com>
2020-09-07 11:14:27 +0000

    added chat component

---
+++ frontend/src/app/modules/group/group-chat/group-chat.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-group-chat',
+  templateUrl: './group-chat.component.html',
+  styleUrls: ['./group-chat.component.sass']
+})
+export class GroupChatComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
