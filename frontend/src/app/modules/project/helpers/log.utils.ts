ca6f44e9a1eb10b4dfb77810b7ec2b182811e026
Volodymyr <volodymyr_bilyk@outlook.com>
2020-09-09 01:05:09 +0300

    Update log.utils.ts

--- frontend/src/app/modules/project/helpers/log.utils.ts
+++ frontend/src/app/modules/project/helpers/log.utils.ts
@@ -1,4 +1,4 @@
 import { IProjectLog } from '@shared/models/project/project-log';
 
-export const formatLog = (log: IProjectLog) =>
-  `[${this.step++} ${log.timestamp} INF] ${log.message}`;
+export const formatLog = (log: IProjectLog, index: number) =>
+  `[${index + 1} ${log.timestamp} INF] ${log.message}`;
