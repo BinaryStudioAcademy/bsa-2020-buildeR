c3431e0b6ac40a2f894428e879cd54524dfd9212
nazar-kotsun <kotsun.nazar69@gmail.com>
2020-09-09 21:41:08 +0300

    Delete methods

--- frontend/src/app/modules/project/project-build/project-build.component.ts
+++ frontend/src/app/modules/project/project-build/project-build.component.ts
@@ -111,8 +111,4 @@ export class ProjectBuildComponent extends BaseComponent implements OnInit {
   openHistory({ id, projectId }: BuildHistory) {
     this.router.navigateByUrl(`/portal/projects/${projectId}/history/${id}`);
   }
-
-  startFirstBuild(){
-    document.getElementById('triggerBuildButton').click();
-  }
 }
