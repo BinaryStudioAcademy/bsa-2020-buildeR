0f71f857f7f9afda7d601d94120609965f68872b
jyuart <yura.palayda@gmail.com>
2020-09-09 08:31:29 +0300

    Add config field to BuildStep model

---
+++ backend/buildeR.Common/DTO/BuildStep/PostBuildStepConfig.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace buildeR.Common.DTO.BuildStep
+{
+    public class PostBuildStepConfig
+    {
+        public string Host { get; set; }
+        public string User { get; set; }
+        public string Password { get; set; }
+        public string OutputDirectory { get; set; }
+    }
+}
