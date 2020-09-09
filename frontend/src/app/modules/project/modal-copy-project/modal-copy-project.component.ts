bf3ff087cf3f30298efd080db41322b6b1f1e28b
nazar-kotsun <kotsun.nazar69@gmail.com>
2020-09-09 17:21:42 +0300

    Add validation name for modal copy-project

--- frontend/src/app/modules/project/modal-copy-project/modal-copy-project.component.ts
+++ frontend/src/app/modules/project/modal-copy-project/modal-copy-project.component.ts
@@ -4,6 +4,10 @@ import { Project } from 'src/app/shared/models/project/project';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import {ToastrNotificationsService} from "@core/services/toastr-notifications.service";
+import {projectNameAsyncValidator} from "@modules/project/validators/project-name.async-validator";
+import {UserService} from "@core/services/user.service";
+import {AuthenticationService} from "@core/services/authentication.service";
+import {User} from "@shared/models/user/user";
 
 @Component({
   selector: 'app-modal-copy-project',
@@ -12,16 +16,21 @@ import {ToastrNotificationsService} from "@core/services/toastr-notifications.se
 })
 export class ModalCopyProjectComponent implements OnInit {
   public copyForm: FormGroup;
-  constructor(private projectService: ProjectService, private activeModal: NgbActiveModal,
-              private toastrService: ToastrNotificationsService) { }
+  constructor(private projectService: ProjectService,
+              private activeModal: NgbActiveModal,
+              private toastrService: ToastrNotificationsService,
+              private authService: AuthenticationService) { }
   @Input() id: number;
   isShowSpinner = false;
+  currentUser: User = {} as User;
   project: Project = {} as Project;
   ngOnInit(): void {
+    this.currentUser = this.authService.getCurrentUser();
     this.projectService.getProjectById(this.id).subscribe((res) => {
       this.project = res; this.copyForm = new FormGroup({
         name: new FormControl(`${this.project.name}` + `-Copy`,
-          [Validators.required]),
+          [Validators.required],
+          projectNameAsyncValidator(this.projectService, this.currentUser)),
         description: new FormControl(this.project.description),
         isPublic: new FormControl(this.project.isPublic.toString())
       });
